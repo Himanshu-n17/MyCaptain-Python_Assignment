305f2fd6ce27c013d8652df30751fd8cd4171565
Himanshu-n17 <144465082+Himanshu-n17@users.noreply.github.com>
2023-09-13 16:54:51 +0530

    Add files via upload

---
+++ A1Q1.py
@@ -0,0 +1,4 @@
+import math
+radius=float(input("Input the radius of the circle : "))
+area=math.pi*radius*radius
+print("Area of circle with radius "+str(radius)+" is : ",str(area))
---
+++ A1Q2.py
@@ -0,0 +1,3 @@
+filename=input("Enter the name of file : ")
+f_extn=filename.split(".")
+print("The extension of the file is:",f_extn[-1])
