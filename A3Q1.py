069b48c5d270a0914b7fad3fc29116b93584c92c
Himanshu-n17 <144465082+Himanshu-n17@users.noreply.github.com>
2023-09-25 22:34:22 +0530

    Add files via upload
    Python code to create a function called most_frequent that takes a string and prints the letters in decreasing order of frequency, Using dictionaries.

---
+++ A3Q1.py
@@ -0,0 +1,17 @@
+def most_frequent(input_string):
+    letter_freq={}
+    
+    input_string=input_string.replace(" ","").lower()
+    for i in input_string:
+        if i.isalpha():
+            if i in letter_freq:
+                letter_freq[i] += 1
+            else:
+                letter_freq[i] = 1
+                
+    sorted_freq=sorted(letter_freq.items(), key=lambda x: x[1], reverse=True)
+    
+    for a,b in sorted_freq:
+        print(f"{a} : {b}")
+
+most_frequent("MISSISSIPPI")
